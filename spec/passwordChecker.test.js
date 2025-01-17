985b6d8d3cc8f124ba1f55030875e6772836c457
axel-lefranc-vinci <axel.lefranc@student.vinci.be>
2025-01-17 17:32:54 +0100

    ADD hasDigit function and corresponding tests

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -1,5 +1,6 @@
 import { hasMinimumLength,
-    hasSpecialCharacter } from '../src/passwordChecker.js';
+    hasSpecialCharacter,
+    hasDigit } from '../src/passwordChecker.js';
 
 describe('hasMinimumLength', () => {
     it('should return true for passwords with at least 8 characters', () => {
@@ -25,3 +26,16 @@ describe('hasSpecialCharacter', () => {
         expect(hasSpecialCharacter('Password')).toBe(false);
     });
 });
+
+describe('hasDigit', () => {
+    it('should return true for passwords with at least one digit', () => {
+        expect(hasDigit('abc1')).toBe(true);
+        expect(hasDigit('Password123')).toBe(true);
+    });
+
+    it('should return false for passwords without digits', () => {
+        expect(hasDigit('abc!')).toBe(false);
+        expect(hasDigit('Password!')).toBe(false);
+    });
+});
+
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -6,3 +6,8 @@ export function hasSpecialCharacter(password) {
     const specialCharPattern = /[!@#$%^&*(),.?":{}|<>]/;
     return specialCharPattern.test(password);
 }
+
+export function hasDigit(password) {
+    const digitPattern = /\d/;
+    return digitPattern.test(password);
+}
